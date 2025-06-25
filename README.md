ba5521a322be0176d3a70c51c9405ec11dd91f9c
raghad595 <raghadeldieb595@gmail.com>
2025-06-25 04:43:26 +0300

    Started streamlit project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AI-ML-Project
 Application of python, data preprocessing, ML, DL
+# Steps
+1) Setting folders & files.
+2) Setting github.
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+st.title("Machine learning application")
+#input & handle data
+st.header("User input")
+file= st.file_uploader("Upload a CSV file", type=["csv"])
+if st.button("Read"):
+    if file is not None:
+        # Read the CSV file
+        try:
+            data = pd.read_csv(file)
+            st.write("Data loaded successfully!")
+            st.write("Data Preview:", data)
+        except Exception as e:
+            st.error(f"Error loading data: {e}")
+    else:
+        st.error("Please upload a CSV file.")
